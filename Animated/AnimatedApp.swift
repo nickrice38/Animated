235de68b83e16c5eba56ecfd24db3d8595679052
Nick Rice <nick.rice@starlingbank.com>
2022-06-13 15:56:15 +0100

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Nick Rice on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
