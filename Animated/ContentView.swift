92c64c87783a509b36d7c7145057e9f813867bff
Nick Rice <nick.rice@starlingbank.com>
2022-06-20 19:08:38 +0100

    - Custom tab bar - Animated tab bar items - Animated menu icon

--- Animated/ContentView.swift
+++ Animated/ContentView.swift
@@ -6,11 +6,41 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
+    @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    @State var isOpen = false
+    let button = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            switch selectedTab {
+            case .chat:
+                Text("Chat")
+            case .search:
+                Text("Search")
+            case .timer:
+                Text("Timer")
+            case .bell:
+                Text("Bell")
+            case .user:
+                Text("User")
+            }
+            
+            button.view()
+                .frame(width: 44, height: 44)
+                .mask(Circle())
+                .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .onTapGesture {
+                    try? button.setInput("isOpen", value: isOpen)
+                    isOpen.toggle()
+                }
+            
+            TabBar()
+        }
     }
 }
 
